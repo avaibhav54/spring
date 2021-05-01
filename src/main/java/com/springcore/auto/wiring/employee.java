65a0a5482bde9839fe6be11735a2d1b2a3754e7e
Vaibhav Agarwal <vaibhav@vaibhav.localvoal>
2021-05-01 12:36:03 +0530

    first

---
+++ src/main/java/com/springcore/auto/wiring/employee.java
@@ -0,0 +1,30 @@
+package com.springcore.auto.wiring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class employee {
+	
+	private address ad;
+
+	public employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public employee(address ad) {
+		this.ad=ad;
+	}
+
+	@Override
+	public String toString() {
+		return "employee [ad=" + ad + "]";
+	}
+	public address getAd() {
+		return ad;
+	}
+
+	public void setAd(address ad) {
+		this.ad = ad;
+	}
+
+	
+}
